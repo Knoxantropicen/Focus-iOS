30d741e371bbe3e98ba8dd25e6e6b5ddc90b99b4
Yunsheng Tian <knoxtian@gmail.com>
2017-04-18 15:06:16 +0800

    Fix bugs in horizontal scrolling.

--- Focus/PageViewController.swift
+++ Focus/PageViewController.swift
@@ -8,22 +8,35 @@
 
 import UIKit
 
-class PageViewController: UIPageViewController {
+class PageViewController: UIPageViewController, UIScrollViewDelegate {
+    
+    static var currentPage: Int = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         dataSource = self
         
+        for view: UIView in view.subviews {
+            if (view is UIScrollView) {
+                (view as? UIScrollView)?.delegate = self
+                break
+            }
+        }
+        
         let firstViewController = orderedViewControllers[1]
         setViewControllers([firstViewController],
-                               direction: .forward, animated: true, completion: nil)
-
+                           direction: .forward, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if PageViewController.currentPage == 0 && scrollView.contentOffset.x < scrollView.bounds.size.width {
+            scrollView.contentOffset = CGPoint(x: CGFloat(scrollView.bounds.size.width), y: CGFloat(0))
+        }
+        else if PageViewController.currentPage == 2 && scrollView.contentOffset.x > scrollView.bounds.size.width {
+            scrollView.contentOffset = CGPoint(x: CGFloat(scrollView.bounds.size.width), y: CGFloat(0))
+        }
+        
     }
     
     //array to reference the view controllers to page through
@@ -36,24 +49,19 @@ class PageViewController: UIPageViewController {
     private func functionViewController(page: String) -> UIViewController {
         return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier:"\(page)ViewController")
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension PageViewController: UIPageViewControllerDataSource {
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        PageViewController.currentPage -= 1
+        
+        guard PageViewController.currentPage >= 0 else {
+            PageViewController.currentPage = 0
+            return nil
+        }
+        
         guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
             return nil
         }
@@ -72,6 +80,13 @@ extension PageViewController: UIPageViewControllerDataSource {
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        PageViewController.currentPage += 1
+        
+        if PageViewController.currentPage > 2 {
+            PageViewController.currentPage = 2
+        }
+        
         guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
             return nil
         }
@@ -89,5 +104,4 @@ extension PageViewController: UIPageViewControllerDataSource {
         
         return orderedViewControllers[nextIndex]
     }
-    
 }
