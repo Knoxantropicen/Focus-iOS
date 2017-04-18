c04cb8c3148ef194925bc1ae5da445328c11a407
Yunsheng Tian <knoxtian@gmail.com>
2017-04-18 15:46:39 +0800

    Fix bugs to better adapt UI on all platforms.

--- Focus/MainViewController.swift
+++ Focus/MainViewController.swift
@@ -55,15 +55,11 @@ class MainViewController: UIViewController, UITextViewDelegate {
         hideKeyboard()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var topRightOperations: UIStackView!
+    
+    override func viewWillLayoutSubviews() {
+        
     }
-    */
 
+    
 }
